c45a9f2c2a2f654110a1a21970a6c463aa3212b5
jesperpen <62403193+jesperpen@users.noreply.github.com>
2021-05-18 16:12:44 +0200

    login pagina werkt met firebase

---
+++ form.js
@@ -0,0 +1,57 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDnugP-KFlsUNWhfM9s-Vr2prxI3riHlmI",
+    authDomain: "inloggen-1c30a.firebaseapp.com",
+    projectId: "inloggen-1c30a",
+    storageBucket: "inloggen-1c30a.appspot.com",
+    messagingSenderId: "762184412510",
+    appId: "1:762184412510:web:39a8b379874786ba4173cf"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+    const auth = firebase.auth()
+
+    function signUp(){
+
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+
+        const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+
+        alert("signed Up");
+    }
+
+    function signIn(){
+
+        var email = document.getElementById("email");
+        var password = document.getElementById("password");
+
+        const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+        promise.catch(e => alert(e.message));
+
+        alert("signed In" + email.value);
+
+    }
+
+    function signOut(){
+
+        auth.signOut();
+        alert("Signed Out")
+    }
+
+    auth.onAuthStateChanged(function(user){
+
+        if(user){
+
+            var email = user.email;
+            alert("Active user " + email);
+        }
+        else{
+
+            alert("No active user");
+        
+        }
+
+    });
